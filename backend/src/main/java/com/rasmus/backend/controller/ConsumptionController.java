78135e0a1cc77231e2cd2d54692def1092c08841
Rasmus Klaus <rasmusklaus6@gmail.com>
2025-02-10 16:18:26 +0200

    backend correction

--- backend/src/main/java/com/rasmus/backend/controller/ConsumptionController.java
+++ backend/src/main/java/com/rasmus/backend/controller/ConsumptionController.java
@@ -1,14 +1,11 @@
 package com.rasmus.backend.controller;
 
-import com.rasmus.backend.model.Consumption;
 import com.rasmus.backend.service.ConsumptionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.math.BigDecimal;
 import java.time.Month;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,12 +14,6 @@ import java.util.Map;
 public class ConsumptionController {
 
     private final ConsumptionService consumptionService;
-
-    @GetMapping("/all/{meteringId}")
-    public ResponseEntity<List<Consumption>> getAllConsumptions(@PathVariable Long meteringId) {
-        List<Consumption> consumptions = consumptionService.getMonthlyConsumption(meteringId);
-        return ResponseEntity.ok(consumptions);
-    }
 
     @GetMapping("/monthly/{meteringId}")
     public ResponseEntity<Map<Month, BigDecimal>> getMonthlyAverageConsumption(@PathVariable Long meteringId) {
--- backend/src/test/java/com/rasmus/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/rasmus/backend/BackendApplicationTests.java
@@ -1,40 +1,13 @@
 package com.rasmus.backend;
 
-// import java.util.Map;
 import org.junit.jupiter.api.Test;
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-
-// import com.rasmus.backend.repository.ConsumptionRepository;
-// import com.rasmus.backend.service.ConsumptionService;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
-// import static org.mockito.Mockito.when;
-
-// import java.math.BigDecimal;
-// import java.time.Month;
-// import java.util.List;
 
 class BackendApplicationTests {
 
-    // @Mock
-    // private ConsumptionRepository consumptionRepository;
-    // @InjectMocks
-    // private ConsumptionService consumptionService;
     @Test
     void simpleTest() {
         assertTrue(true);
     }
-    //    @Test
-    //  void testGetMonthlyAverageConsumptionWithCost_EmptyData() {
-    //     Long meteringId = 2L;
-
-    //     when(consumptionRepository.findByMeteringPoint_MeteringId(meteringId)).thenReturn(List.of());
-
-    //     Map<Month, BigDecimal> result = consumptionService.getMonthlyAverageConsumptionWithCost(meteringId);
-
-    //     assertTrue(result.isEmpty());
-    // }
-
     
 }
